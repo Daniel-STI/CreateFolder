fd260179c8d42238bb635845ffba57cb3f94530f
Daniel-STI <72584118+Daniel-STI@users.noreply.github.com>
2023-03-24 20:14:10 +0000

    Sat, Mar 25, 2023, 4:14 AM +08:00

---
+++ test.py
@@ -0,0 +1,19 @@
+import pygame
+pygame.init()
+
+game = pygame.display.set_mode((500,500))
+pygame.display.set_caption("Test")
+pygame.display.update()
+pygame.time.delay(5000)
+x=100
+y=100
+height=50
+width=50
+movement=5
+
+run=True
+# while run:
+#     pygame.time.delay(10)
+#     for event in pygame.event.get():
+#         if event.type == pygame.quit():
+#             run==False
